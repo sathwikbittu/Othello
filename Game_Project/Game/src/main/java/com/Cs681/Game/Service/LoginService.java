438baf79b69d1ac0ab4a10241f3e25b2a03640e0
Sathwik Vemulapally <48754722+sathwikbittu@users.noreply.github.com>
2023-05-10 03:01:40 -0400

    Create LoginService.java

---
+++ Game_Project/Game/src/main/java/com/Cs681/Game/Service/LoginService.java
@@ -0,0 +1,28 @@
+package com.Cs681.Game.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Cs681.Game.Model.User;
+import com.Cs681.Game.Repo.UserRepo;
+
+@Service
+public class LoginService{
+	@Autowired
+	UserRepo userRepo;
+	public Boolean loginCheck(String userName, String userPassword) {
+		List<User> users = userRepo.findByUserName(userName);
+		User user = null;
+		if(!users.isEmpty()) {
+			user = users.get(0);
+		}
+		if(user!=null && userPassword.equals(user.getUserPassword())) {
+			return true;
+		}
+		return false;
+	
+
+}
+}

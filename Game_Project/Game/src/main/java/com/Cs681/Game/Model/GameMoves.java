0781e3a00b85667aa263ffb81a3d9b9812357d27
NishithVadlamudi <nishith.vadlamudi@gmail.com>
2023-05-11 12:26:03 -0400

    Game move entity update

--- Game_Project/Game/src/main/java/com/Cs681/Game/Model/GameMoves.java
+++ Game_Project/Game/src/main/java/com/Cs681/Game/Model/GameMoves.java
@@ -2,10 +2,12 @@ package com.Cs681.Game.Model;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class GameMoves {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

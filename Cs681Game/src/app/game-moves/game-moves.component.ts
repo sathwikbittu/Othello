5ea23ac9634d6dbaa54ba0f5917fbdd50ac23d69
NishithVadlamudi <nishith.vadlamudi@gmail.com>
2023-05-12 13:08:37 -0400

    pdf download for game moves

--- Cs681Game/src/app/game-moves/game-moves.component.ts
+++ Cs681Game/src/app/game-moves/game-moves.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { GameMoves } from '../game-moves';
 import { HttpClient } from '@angular/common/http';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
+// Register fonts with pdfMake
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-game-moves',
   templateUrl: './game-moves.component.html',
@@ -10,13 +14,24 @@ import { HttpClient } from '@angular/common/http';
 export class GameMovesComponent implements OnInit {
   games: GameMoves[] = [];
   player: any;
+  searchTerm: string = '';
+  filteredGames: GameMoves[] = [];
+  
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.player = localStorage.getItem("userName") || '{}';
     this.getGames();
+    this.searchGames();
   }
+  // Inside the component class
+
+
+  searchGames(): void {
+    this.filteredGames = this.games.filter(game => game.id.toString().includes(this.searchTerm));
+  }
+
 
   getGames() {
     this.http.get<GameMoves[]>('http://localhost:8080/getGames').subscribe(
@@ -32,4 +47,29 @@ export class GameMovesComponent implements OnInit {
       }
     );
   }
+  downloadGameMovesAsPDF(game: GameMoves): void {
+    const docDefinition = {
+      content: [
+        { text: `Game ${game.id} Moves`, style: 'header' },
+        { text: '\n' }, // Add some spacing
+  
+        ...game.moves.map(move => {
+          const playerLabel = move.player === this.player ? 'You' : move.player;
+          return { text: `${playerLabel} moved to row ${move.moveRow}, column ${move.moveColumn}` };
+        })
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          marginBottom: 10
+        }
+      }
+    };
+  
+    const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+    pdfDocGenerator.download(`game_${game.id}_moves.pdf`);
+  }
+
 }
+
